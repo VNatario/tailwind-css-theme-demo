1e9f1d271210ac5b76e3a4c65daf092836247680
Vitor Natario <natariovitor@gmail.com>
2023-12-20 21:55:53 -0300

    feat: use new component and add styles in home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,16 @@
 'use client'
 
 import { useTheme } from './contextTheme'
+import { TimeoutComponent } from './wrapper'
 
 export default function Home() {
   const { theme, toggleTheme } = useTheme()
 
   return (
     <div className={theme}>
+      <TimeoutComponent />
       <div className="min-h-screen flex flex-col justify-center items-center gap-10 bg-indigo-50 dark:bg-slate-900 selection:bg-violet-400 dark:selection:bg-violet-700">
-        <div className="max-w-[960px] p-5 bg-slate-100 dark:bg-zinc-900 rounded-lg flex gap-4 border border-zinc-900 dark:border-zinc-200 hover:shadow-cartom hover:dark:shadow-cartom-dark duration-300">
+        <div className="max-w-[960px] p-5 bg-slate-100 dark:bg-zinc-900 rounded-lg flex gap-4 border border-zinc-900 dark:border-zinc-200 hover:shadow-cartom hover:dark:shadow-cartom-dark duration-300 cursor-pointer">
           <div className="w-24 h-24 bg-indigo-500 rounded-lg" />
           <span className="flex-1 flex justify-center items-center">
             <p className="text-justify text dark:text-slate-200">
