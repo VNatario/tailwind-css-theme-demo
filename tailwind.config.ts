d884e8165b80374e43b2559d3332ea10fad152e8
Vitor Natario <natariovitor@gmail.com>
2023-12-20 21:54:49 -0300

    styles: add new styles

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,20 @@ const config: Config = {
         cartom: '15px -12px 0px 0px rgba(0,0,0,1)',
         'cartom-dark': '15px -12px 0px 0px rgba(99, 102, 241, 0.5)',
       },
+      keyframes: {
+        'rotation-break': {
+          '0%': { transform: 'rotate(0)' },
+          '25%': { transform: 'rotate(90deg)' },
+          '50%': { transform: 'rotate(180deg)' },
+          '75%': { transform: 'rotate(270deg)' },
+          '100%': { transform: 'rotate(360deg)' },
+        },
+      },
+      animation: {
+        'rotation-break': 'rotation-break 3s ease-in-out infinite alternate',
+        'rotation-break-reverse':
+          'rotation-break 3s ease-in-out infinite alternate-reverse',
+      },
     },
   },
   plugins: [],
