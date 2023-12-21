60223aad778fbc38875ce10e383276054c8213b5
Vitor Natario <natariovitor@gmail.com>
2023-12-20 21:56:02 -0300

    feat: add loading page

---
+++ src/app/loading.tsx
@@ -0,0 +1,7 @@
+export default function Loading() {
+  return (
+    <div className="bg-slate-900 w-full h-screen flex justify-center items-center">
+      <span className="w-12 h-12 inline-block relative  after:content-[''] after:box-border after:w-12 after:h-12 after:border-2 after:border-indigo-700 after:absolute after:left-0 after:top-0 before:content-[''] before:box-border before:w-12 before:h-12 before:border-2 before:border-slate-200 before:absolute before:left-0 before:top-0 before:animate-rotation-break after:animate-rotation-break-reverse" />
+    </div>
+  )
+}
